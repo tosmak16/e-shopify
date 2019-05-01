0ed8bd64c47617de51ee5d65711b9b4ed08c547a
Tosmak <tosmak16@gmail.com>
2019-05-01 23:39:19 +0100

    Implement update customer address

---
+++ server/controllers/customers/updateCustomerAddress.js
@@ -0,0 +1,27 @@
+import CustomerService from '../../services/database/CustomerService';
+import normalizeCustomerData from '../../utils/normalizeCustomerData';
+
+const updateCustomerAddress = async (req, res) => {
+  const { customer_id } = req.decoded;
+  const { email } = req.body;
+  const fetchedCustomer = await CustomerService.findById(customer_id);
+
+  if (fetchedCustomer === null) {
+    return res.status(400).send({
+      error: {
+        status: 400,
+        code: 'USR_02',
+        message: 'Customer does not exist',
+        field: 'NoCustomer'
+      }
+    });
+  }
+
+  const updatedCustomer = await CustomerService.update(fetchedCustomer, req.body);
+
+  const customer = normalizeCustomerData(updatedCustomer);
+
+  return res.status(200).send(customer);
+};
+
+export default updateCustomerAddress;
