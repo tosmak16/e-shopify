aeaffcbe612e7e91fe3af6cf99b7c8db95216577
Tosmak <tosmak16@gmail.com>
2019-05-01 23:10:09 +0100

    Add update method to CustomerService

--- server/services/database/CustomerService.js
+++ server/services/database/CustomerService.js
@@ -15,6 +15,21 @@ class CustomerService {
     const result = await Customer.findByPk(id);
     return result;
   }
+
+  static async update(customer, newCustormerData) {
+    const { name, email, day_phone, password, eve_phone, mob_phone } = newCustormerData;
+
+    await customer.update({
+      name: name || customer.name,
+      email: email || customer.email,
+      day_phone: day_phone || customer.day_phone,
+      eve_phone: eve_phone || customer.eve_phone,
+      mob_phone: mob_phone || customer.mob_phone,
+      password: password || customer.password
+    });
+
+    return customer;
+  }
 }
 
 export default CustomerService;
