9660f2e98b00473384481435eb11a661d602b760
Tosmak <tosmak16@gmail.com>
2019-05-01 23:10:24 +0100

    Implement update customer

--- server/controllers/customers/index.js
+++ server/controllers/customers/index.js
@@ -1,5 +1,6 @@
 import signIn from './signIn';
 import signUp from './signUp';
 import getCustomer from './getCustomer';
+import updateCustomer from './updateCustomer';
 
-export { signIn, signUp, getCustomer };
+export { signIn, signUp, getCustomer, updateCustomer };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,18 @@
 import express from 'express';
 
 import tokenAuthMiddleware from '../middlewares/authenticationMiddleware/tokenAuthMiddleware';
-import { validateSignUpData, validateLoginData } from '../middlewares/validationMiddlewares';
-import { signIn, signUp, getCustomer } from '../controllers/customers';
+import {
+  validateSignUpData,
+  validateLoginData,
+  validateUpdateCustomerData
+} from '../middlewares/validationMiddlewares';
+import { signIn, signUp, getCustomer, updateCustomer } from '../controllers/customers';
 
 const router = express.Router();
 
 router.post('/customers', validateSignUpData, signUp);
 router.post('/customers/login', validateLoginData, signIn);
 router.get('/customer', tokenAuthMiddleware, getCustomer);
+router.put('/customer', tokenAuthMiddleware, validateUpdateCustomerData, updateCustomer);
 
 export default router;
